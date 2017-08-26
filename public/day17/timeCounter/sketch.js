a32424a24384e7569d8bed856d14e2cb693684e8
econgamer <econgamer@gmail.com>
2017-08-26 13:41:39 +0800

    day 17 - time counter- upload

---
+++ public/day17/timeCounter/sketch.js
@@ -0,0 +1,91 @@
+var input;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var totalSeconds;
+var soundFile;
+
+
+
+function preload(){
+  soundFormats('ogg', 'mp3');
+  soundFile = loadSound(`alarm.mp3`);
+}
+
+function setup(){
+
+
+  createCanvas(windowWidth,windowHeight);
+  background(56);
+
+  var params = getURLParams();
+  console.log(params);
+
+  if(params.sec){
+    seconds = Number(params.sec);
+  }else{
+    seconds = 0;
+    console.log('second clean');
+  }
+
+  if(params.min){
+    minutes = Number(params.min);
+  }else{
+    minutes = 0;
+  }
+
+  if(params.hr){
+    hours = Number(params.hr);
+  }else{
+    hours = 0;
+  }
+
+
+
+
+
+  totalSeconds = seconds + minutes * 60 + hours * 60 * 60;
+  console.log(totalSeconds);
+}
+
+function draw() {
+
+  background(255);
+
+
+
+
+  textSize(200);
+  textStyle(BOLD)
+  textAlign(CENTER);
+
+
+  text(`${nf(hours,2)}:${nf(minutes,2)}:${nf(seconds,2)}`, width/2, height/2);
+}
+
+
+var interval = setInterval(countDown, 1000);
+
+function countDown(){
+
+  if(totalSeconds != 0){
+    totalSeconds -= 1;
+
+    hours = Math.floor(totalSeconds / (60 * 60));
+    console.log(totalSeconds);
+
+
+    // minutes = Math.floor((totalSeconds / 60));
+    minutes = Math.floor((totalSeconds - (hours * 60 * 60)) / 60);
+    seconds = Math.floor(totalSeconds - (hours * 60 * 60) - (minutes * 60));
+
+    console.log('Hours' + hours);
+    console.log('mins' + minutes);
+    console.log('seconds' + seconds);
+  }else{
+    clearInterval(interval);
+    soundFile.play();
+    console.log('clean');
+  }
+
+}
